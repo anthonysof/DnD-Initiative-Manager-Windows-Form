d81a03070da7b85b0ad8bf1ed666c84abaaf00a6
anthonysof <antonis.sofras@gmail.com>
2018-12-07 12:48:44 +0200

    first commit

---
+++ Initiatiave Manager/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace Initiatiave_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(201, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Character";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(95, 96);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(176, 258);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(408, 96);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(176, 258);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(408, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Roll Initiative";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(201, 660);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(342, 660);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(34, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(202, 688);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 32);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Add DM Creature";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(408, 462);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(176, 258);
+            this.richTextBox3.TabIndex = 9;
+            this.richTextBox3.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 616);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 104);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Clear DM Creatures";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 538);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(164, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(182, 462);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 49);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Advantage";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(182, 525);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(105, 49);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Normal";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(182, 588);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(105, 49);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Disadvantage";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Initiative Bonus";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(369, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Choose a character then press a button to set advantage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(198, 640);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(339, 640);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 17);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Initiative:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(10, 366);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(164, 24);
+            this.comboBox2.TabIndex = 20;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(182, 360);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(162, 35);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Remove Character";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(408, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Turn of: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(477, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(408, 389);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 17);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Round: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(472, 389);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 25;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(408, 410);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(176, 46);
+            this.button9.TabIndex = 26;
+            this.button9.Text = "Start Encounter";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Visible = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 748);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Initiative Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Initiatiave Manager/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Initiatiave_Manager
+{
+    public partial class Form1 : Form
+    {
+        static Random rnd = new Random();
+        List<Player> playerList;
+        List<Player> dmList;
+        List<string> namelist;
+        IEnumerable<Player> paixtoures;
+        int roundCount = 1;
+        
+        class Player
+        {
+            private int initBonus;
+            private int hasAdvantage;
+            private string name;
+            private int finalInit;
+
+            public Player(string name, int initBonus)
+            {
+                this.name = name;
+                this.initBonus = initBonus;
+                this.hasAdvantage = 0;
+            }
+            public string Name
+            {
+                get => name;
+                set => name = value;
+            }
+
+            public int FinalInit
+            {
+                get => finalInit;
+                set => finalInit = value;
+            }
+            public int InitiativeBonus
+            {
+                get => initBonus;
+                set => initBonus = value;
+            }
+
+            public int Advantage
+            {
+                get
+                {
+                    if (hasAdvantage == 0)
+                    {
+                        return 0;
+                    }
+                    else if (hasAdvantage == 1)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                set
+                {
+                    if (value < -1 || value > 1)
+                    {
+                        throw new ArgumentOutOfRangeException($"{nameof(value)} must be -1, 0 or 1");
+                    }
+                    else
+                        hasAdvantage = value;
+                }
+            }
+
+            public int rollInitiative()
+            {
+                if (hasAdvantage == 0)
+                {
+                    return rnd.Next(1, 20) + this.initBonus;
+                }
+                else if (hasAdvantage == 1)
+                {
+                    int init1, init2;
+                    init1 = rnd.Next(1, 20);
+                    init2 = rnd.Next(1, 20);
+                    return Math.Max(init1, init2) + this.initBonus;
+                }
+                else
+                {
+                    int init1, init2;
+                    init1 = rnd.Next(1, 20);
+                    init2 = rnd.Next(1, 20);
+                    return Math.Min(init1, init2) + this.initBonus;
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            playerList = new List<Player>();
+            dmList = new List<Player>();
+            namelist = new List<string>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Player paixtis;
+            int temp;
+            int.TryParse(textBox2.Text, out temp);
+            playerList.Add(paixtis = new Player(textBox1.Text, temp));
+            namelist.Add(paixtis.Name);
+            richTextBox1.Text += paixtis.Name + " " + paixtis.InitiativeBonus+"\n";
+            comboBox1.Items.Add(paixtis.Name);
+            comboBox2.Items.Add(paixtis.Name);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(button2.Text == "End Encounter")
+            {
+                button2.Text = "Roll Initiative";
+                roundCount = 1;
+                richTextBox2.Text = "";
+                label7.Text = "";
+                label9.Text = "";
+                button9.Text = "Start Encounter";
+                button9.Visible = false;
+                return;
+             
+            }
+            button9.Visible = true;
+            roundCount = 1;
+            richTextBox2.Text = "";
+            //var newList = a.Concat(b);
+            foreach (Player item in playerList)
+            {
+                item.FinalInit = item.rollInitiative();
+            }
+            var newList = playerList.Concat(dmList);
+            paixtoures =  newList.OrderByDescending(x => x.FinalInit);
+            foreach (Player item in paixtoures)
+            {
+                richTextBox2.Text += item.Name + " " + item.FinalInit + "\n";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Player creature;
+            int temp;
+            int.TryParse(textBox4.Text, out temp);
+            dmList.Add(creature = new Player(textBox3.Text, 0));
+            creature.FinalInit = temp;
+            richTextBox3.Text += creature.Name + " " + creature.FinalInit + "\n";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Text = "";
+            dmList.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() != "")
+            {
+                String name = comboBox1.SelectedItem.ToString();
+                Player paixtis = playerList.Find(i => i.Name == name);
+                paixtis.Advantage = 1;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() != "")
+            {
+                String name = comboBox1.SelectedItem.ToString();
+                Player paixtis = playerList.Find(i => i.Name == name);
+                paixtis.Advantage = 0;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() != "")
+            {
+                String name = comboBox1.SelectedItem.ToString();
+                Player paixtis = playerList.Find(i => i.Name == name);
+                paixtis.Advantage = -1;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedItem.ToString() != "")
+            {
+                String name = comboBox2.SelectedItem.ToString();
+                Player paixtis = playerList.Find(i => i.Name == name);
+                playerList.Remove(paixtis);
+                richTextBox1.Text = "";
+                foreach (Player item in playerList)
+                {
+                    richTextBox1.Text += item.Name + " " + item.InitiativeBonus + "\n";
+                }
+                comboBox1.Items.Remove(paixtis.Name);
+                comboBox2.Items.Remove(paixtis.Name);
+            }
+            else
+                return;
+        }
+        int index = 0;
+        private void button9_Click(object sender, EventArgs e)
+        {
+            button9.Text = "Next...";
+            List<Player> tempList = paixtoures.ToList<Player>();
+            label7.Text = tempList.ElementAt(index).Name.ToString();
+            button2.Text = "End Encounter";
+            
+            index++;
+            
+            label9.Text = roundCount.ToString();
+            if (index == tempList.Count)
+            {
+                index = 0;
+                roundCount++;
+            }
+        }
+    }
+}
